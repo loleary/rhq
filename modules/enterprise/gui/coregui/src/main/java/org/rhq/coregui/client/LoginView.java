831d60f7b4ed1ce5b9c583c97c5898cf9c5fc3b1
Jirka Kremser <jkremser@redhat.com>
2014-06-10 20:53:56 +0200

    Sprinkling the login process with some JavaScript love.

--- modules/enterprise/gui/coregui/src/main/java/org/rhq/coregui/client/LoginView.java
+++ modules/enterprise/gui/coregui/src/main/java/org/rhq/coregui/client/LoginView.java
@@ -677,6 +677,14 @@ public class LoginView extends Canvas {
       });
     }-*/;
     
+    public native static boolean isKeycloakEnabled() /*-{
+        return $wnd.keycloak;
+    }-*/;
+    
+    public native static boolean isKeycloakReady() /*-{
+        return typeof($wnd.kcReady) != "undefined" && $wnd.kcReady;
+    }-*/;
+    
     public static boolean isLoginView() {
         return isLoginView && com.google.gwt.user.client.Window.Location.getHref().contains(LOGIN_VIEW);
     }
